772702c53e0c0561f456d2381bdbcf8100ec4f96
NPL Codes <157637527+alvinleon1606@users.noreply.github.com>
2024-02-13 16:59:28 +0200

    js/Dom implementation

--- script.js
+++ script.js
@@ -6,3 +6,63 @@ humbergerBtn.addEventListener("click", () => {
     humbergerBtn.classList.toggle('active');
     menus.classList.toggle('active');
 })
+
+// Hero section
+
+const modal = document.querySelector('.modal');
+const modalContent = document.querySelector('.modal-content');
+const videoPlayIcon = document.querySelector('.video-play-icon');
+
+
+videoPlayIcon.addEventListener('click', () =>{
+    modal.style.display = "block"
+})
+
+
+// close 
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+window.onclick = function(e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+
+//   About Me
+// +++++++++++++++++ Background ++++++++++++++++
+
+var educationInfo = document.querySelector('.education-background');
+var personalInfo = document.querySelector('.expirience-background');
+// Buttons
+var educationBtn = document.querySelector('.education');
+var personalBtn = document.querySelector('.experience');
+
+document.querySelector('.education').addEventListener('click', function() {
+    
+    educationInfo.style.display = 'grid';
+    educationBtn.classList.add('active');
+    personalInfo.style.display = 'none';
+    personalBtn.classList.remove('active');
+  });
+
+  window.onload=() =>{
+    educationInfo.style.display = 'grid';
+    educationBtn.classList.add('active');
+    personalInfo.style.display = 'none';
+    personalBtn.classList.remove('active');
+  }
+  
+  document.querySelector('.experience').addEventListener('click', function() {
+    var educationInfo = document.querySelector('.education-background');
+    var personalInfo = document.querySelector('.expirience-background');
+    
+    educationInfo.style.display = 'none';
+    educationBtn.classList.remove('active');
+    personalInfo.style.display = 'grid';
+    personalBtn.classList.add('active');
+  });
