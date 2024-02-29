523213439d93c202d3b569fe7ba3606ec8d24754
NPL Codes <157637527+alvinleon1606@users.noreply.github.com>
2024-02-29 08:43:27 +0200

    setup environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
+// server development
+import express from 'express';
+import dotenv from 'dotenv';
 
+
+const app = express()
+
+dotenv.config()
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, ()=> console.log(`Server is running on ${port} ...`));
