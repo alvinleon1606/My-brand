124428264c013498ec7dc9cf8aed53587e8b8778
NPL Codes <157637527+alvinleon1606@users.noreply.github.com>
2024-02-29 09:50:38 +0200

    Project setup(TS + Node + Express + MongoDB)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,14 +3,16 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/database';
 
-
-
 const app = express()
-connectDB
+
+app.use(express.json())
+connectDB()
 
 dotenv.config()
 
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.listen(port, ()=> console.log(`Server is running on ${port} ...`));
+app.listen(port, ()=> {
+    console.log(`Server is running on ${port} ...`);
+})
