b64de4fe4f1a71c46833fe154f25f91cc06a1a75
NPL Codes <157637527+alvinleon1606@users.noreply.github.com>
2024-03-29 21:06:42 +0200

    messages integration

--- js/Contact.js
+++ js/Contact.js
@@ -1,67 +1,51 @@
-// Contact Me
-document.addEventListener('DOMContentLoaded', () =>{
-    const crentials = document.querySelector('#for-logged-user-only');
+document.addEventListener('DOMContentLoaded', () => {
+    const credentials = document.querySelector('#for-logged-user-only');
     const loggedInUserSession = JSON.parse(localStorage.getItem('LoggedUserInfo'));
     if (loggedInUserSession) {
-        crentials.style.display = 'none';
+        credentials.style.display = 'none';
     }
-    
-    //Get logged In user
+
     let user = JSON.parse(localStorage.getItem('LoggedUserInfo'));
 
-    const MessageStore = JSON.parse(localStorage.getItem("Messages")) || [];
+    // on focus to any field
+    if (!user) {
+        document.querySelectorAll('input, textarea').forEach((element) => {
+            element.addEventListener('focus', () => {
+                window.location.href = './Login.html';
+            });
+        });
+    }
 
-    document.getElementById('contact-form').addEventListener('submit', (e) =>{
-        e.preventDefault()
+    document.getElementById('contact-form-sending').addEventListener('submit', async (e) => {
+        e.preventDefault();
 
-        // Un registered User Message
-        const names = document.getElementById('names').value;
-        const email = document.getElementById('email').value;
-        const subject = document.getElementById('subject').value;
-        const telephone = document.getElementById('telephone').value;
-        const Message = document.getElementById('message').value;
-        // find if user has recently sent message
+        const message = document.getElementById('message').value;
+        const newMessage = {
+            names: user?.user.firstName+" "+user?.user.secondName,
+            email: user?.user.email,
+            message: message
+        };
 
-        if (!user) {
-            const recentMessageIndex = MessageStore.findIndex((recent) => recent.userEmail === email);
-            const recentMessages = MessageStore[recentMessageIndex];
-            if (recentMessageIndex !== -1) {
-                recentMessages.message.push(Message);
-            }else{
-                //
-                // Un registered User Message 
-                const newMassege = {
-                    msgId: Date.now(),
-                    userNames: names,
-                    userEmail: email,
-                    message: [Message],
-                    subject: subject,
-                    telephone: telephone,
-                    read: false
-                }
-                MessageStore.push(newMassege);
+        // Send the message 
+        try {
+            const response = await fetch('http://localhost:8080/messages/new', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newMessage)
+            });
+
+            if (response.ok) {
+                alert("Message sent successfully")
+                console.log('Message sent successfully');
+                // Clear the message input
+                document.getElementById('message').value = "";
+            } else {
+                console.error('Failed to send message');
             }
+        } catch (error) {
+            console.error('Failed to send message:', error);
         }
-        const recentMessageIndex = MessageStore.findIndex((recent) => recent.userEmail === user.email);
-        const recentMessages = MessageStore[recentMessageIndex];
-
-        if (recentMessageIndex !== -1) {
-            recentMessages.message.push(Message);
-        }else{
-            // Message object
-            const newMassege = {
-                msgId: Date.now(),
-                userNames: user.names,
-                userEmail: user.email,
-                message: [Message],
-                read: false
-            }
-            MessageStore.push(newMassege);
-        }
-        // save data
-        localStorage.setItem("Messages", JSON.stringify(MessageStore));
-        document.getElementById('message').value = ""
-    })
-})
-
- 
+    });
+});
